56650cfab70e57716bebbe98fbebfeaa04c01ffb
chendjou237 <chenxhenor@gmail.com>
2024-06-19 16:17:27 +0200

    create booking

--- app/bookings/[id]/page.tsx
+++ app/bookings/[id]/page.tsx
@@ -1,6 +1,17 @@
+import { CreateBooking } from "@/components/component/create-booking";
+import { Gallery } from "@/components/component/gallery";
+import { Hero } from "@/components/component/hero";
 
 export default function Page({params}: {params:{id: string}}) {
 
-  return (<center>{params.id}</center>)
+  return (
+    <main className="p-16">
+     <Hero />
+  <div className=" flex min-h-screen flex-col items-center justify-between">
+     <Gallery />
+    <CreateBooking/>
+  </div>
+    </main>
+  )
   
 }
