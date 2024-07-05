bad4bd4bc3ee834083f4f12f49804d477ae51acc
CHENDJOU CHENDJOU HONORE <chenxhenor@gmail.com>
2024-07-05 18:03:44 +0000

    hi

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -18,21 +18,18 @@ export default function Layout   ({
    return (
       <div className="flex min-h-screen  w-full flex-col bg-muted/40">
          <header className="sm:hidden sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-        <Link href="#" className="flex items-center gap-2" prefetch={false}>
-          <MountainIcon className="h-6 w-6" />
-          <span className="text-lg font-semibold">Decor Booking</span>
-        </Link>
+       
         <nav className="ml-auto flex items-center gap-4 sm:gap-6">
-          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+          <Link href="/dashboard/" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
             Bookings
           </Link>
-          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+          <Link href="/dashboard/users" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
             Customers
           </Link>
-          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+          <Link href="/dashboard/services" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
             Services
           </Link>
-          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+          <Link href="/dashboard/settings" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
             Settings
           </Link>
         </nav>
